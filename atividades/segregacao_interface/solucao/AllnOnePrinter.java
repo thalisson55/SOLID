cea1f1dd9d6d3a777b601a60c6f48f9d573baea9
thalisson55 <117370237+thalisson55@users.noreply.github.com>
2023-11-01 11:20:45 -0300

    finalização

--- atividades/segregacao_interface/solucao/AllnOnePrinter.java
+++ atividades/segregacao_interface/solucao/AllnOnePrinter.java
@@ -1,4 +1,4 @@
-public class AllInOnePrinter implements Machine {
+public class AllInOnePrinter implements Printer, Scanner {
     @Override
     public void print() {
         // Implementação do método print
